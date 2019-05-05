a1383200f69d4225e3463c473c14d12140fd8bd3
zhaobendong <zhaobendong@jianlc.com>
2019-05-05 13:53:43 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,19 @@
+# webpack-batch-compress-pic
+基于webpack批量压缩图片，对于webpack，图片，js，css文件都是模块之一，所以webpack的入口可以是图片，js，css等资源
+
+## 注意
++ 图片放在根目录static文件下
++ webpack4对应的file-loader的版本是3.x，而webpack3.x对应的file-loader的版本是2，切记，否则可能会出错
+
+## webpack4.x配置
+[webpack-batch-compress-pic](https://github.com/zdmission/webpack-batch-compress-pic)
+
+## 输出
++ 输出目录是dist
+
+## 命令
+```bash
+npm run imgmin
+# 或者
+yarn imgmin
+```
---
+++ webpack.config-imgmin.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const glob = require('glob');
+
+// 保存图片数据
+const arrImg = [];
+glob.sync('./static/**/*.{png,jpeg,jpg,gif,svg,ico}').forEach(entry => {
+    arrImg.push(entry);
+});
+
+// 在webpack的处理流中操作文件
+function fileChunck(options) {
+    this.options = options;
+}
+
+fileChunck.prototype.apply = function (compiler) {
+    const delFileNameArr = this.options.del ? this.options.del : []; // 删除指定文件
+    const copyArr = this.options.copy ? this.options.copy : []; // 移动指定文件
+    compiler.plugin('emit', function (compilation, callback) {
+        Object.keys(compilation.assets).filter((filePath) => {
+            /* 删除指定文件 */
+            if (delFileNameArr.length > 0) {
+                const targetFilePathArr = filePath.split('?')[0].split('/'),
+                    targetFilePath = targetFilePathArr[targetFilePathArr.length - 1];
+                if (delFileNameArr.indexOf(targetFilePath) >= 0) {
+                    delete compilation.assets[filePath];
+                }
+            }
+
+            /* 复制指定文件 */
+            if (copyArr.length > 0) {
+                copyArr.filter((obj) => {
+                    if (obj.from.indexOf('.html') != -1) {
+                        if (filePath === obj.from) {
+                            compilation.assets[obj.to] = compilation.assets[filePath];
+                        }
+                    } else {
+                        if (filePath.indexOf('.html') != -1 && filePath.indexOf(obj.from) != -1) {
+                            const targetStr = obj.to + filePath.replace(obj.from, '');
+                            compilation.assets[targetStr] = compilation.assets[filePath];
+                        }
+                    }
+                    return obj;
+                });
+            }
+
+            return (filePath);
+        });
+        callback();
+    });
+};
+
+module.exports = {
+    entry: arrImg,
+    context: path.resolve(__dirname, './'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    resolve: {
+    // 后缀名
+        extensions: ['.js', '.css', '.vue']
+    },
+    module: {
+        rules: [
+            // image
+            {
+                test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1, // 小于5bit的图片自动转成base64格式，并且不会存在实体图片
+                            name: '[path][name].[ext]?[hash:8]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            optipng: {
+                                optimizationLevel: 7
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new fileChunck({del: ['main.js']})
+    ]
+};
